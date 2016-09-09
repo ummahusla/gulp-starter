bbcaefb66f8729d79a456fb63d55827b6a42119c
Edvins Antonovs <edvins.antonovs@british-gymnastics.org>
2016-09-09 09:26:58 +0100

    Added browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+})
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-})
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
 
-gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-})
+gulp.task('watch', ['browser-sync', 'sass'], function (){
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+});
